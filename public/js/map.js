211dc21831867ad9df8f7340918bdf074fb1c508
R Sánchez <rene.as.himself@gmail.com>
2020-07-25 18:11:11 -0500

    Plotting Points

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,5 @@
-import dotenv from 'dotenv'
-
-dotenv.config({
-  path: '../../config/.env.dev',
-})
-
-const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN
-console.log('MAPBOX_ACCESS_TOKEN', MAPBOX_ACCESS_TOKEN)
+const MAPBOX_ACCESS_TOKEN =
+  'pk.eyJ1Ijoic29kYWgxMTciLCJhIjoiY2tkMjgwMWt2MWFydjJxcXJ4Y3c3bTh1cCJ9.RK88I6XiiOu3j6cJQejuLA'
 
 mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN
 
@@ -15,3 +9,37 @@ const map = new mapboxgl.Map({
   zoom: 9,
   center: [-71.157895, 42.707741],
 })
+
+function loadMap() {
+  map.on('load', function () {
+    map.addLayer({
+      id: 'points',
+      type: 'symbol',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [-71.157895, 42.707741],
+            },
+            properties: {
+              storeId: '0001',
+              icon: 'shop',
+            },
+          },
+        },
+      },
+      layout: {
+        'icon-image': '{icon}-15',
+        'icon-size': 1.5,
+        'text-field': '{storeId}',
+        'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+        'text-offset': [0, 0.9],
+        'text-anchor': 'top',
+      },
+    })
+  })
+}
